d6418b29e4759f31e33856c0a35d29f6643ca2bf
Christopher J. Madsen <cjm@cjmweb.net>
2009-10-20 01:16:21 -0500

    Replaced cd_with_history with dirhistory_check   Bash 4.0's autocd feature can't be intercepted with an alias,   so we have to add our check to $PROMPT_COMMAND

--- cd.sh
+++ cd.sh
@@ -1,27 +1,34 @@
 # $Id$
 
-function cd_with_history () {
-  builtin cd "$@"
+DIRHISTORY_PWD="${PWD}"
+
+function dirhistory_check () {
   local status=$?
 
-  /home/cjm/apps/dirhistory/dirchange /dev/shm/cjm-dirs "${OLDPWD}" "${PWD}"
+  if [ "${PWD}" != "${DIRHISTORY_PWD}" ] ; then
+    /home/cjm/apps/dirhistory/dirchange /dev/shm/cjm-dirs \
+	"${DIRHISTORY_PWD}" "${PWD}"
+    DIRHISTORY_PWD="${PWD}"
+  fi
 
   return ${status}
-} # end cd_with_history
+} # end dirhistory_check
 
 function cd_menu () {
   local dest=`/home/cjm/apps/dirhistory/dirpick /dev/shm/cjm-dirs /dev/shm/cjm-dir-list`
   if [ -d "${dest}" ]; then
-    cd_with_history "${dest}"
+    cd "${dest}"
 #    echo "Now in ${PWD}"
 #    echo -en "\\033]0;Testing ${USER}@${HOSTNAME}:${PWD}(${dest})\\007"
   fi
 } # end cd_menu
 
-alias cd=cd_with_history
-
 #bind -x $'"\201":cd_menu'
 #bind '"\e[6;5~":'$'"\201"'
+
+# Install dirhistory_check into $PROMPT_COMMAND:
+[[ "${PROMPT_COMMAND}" =~ "dirhistory_check" ]] \
+ || PROMPT_COMMAND="dirhistory_check ; ${PROMPT_COMMAND}"
 
 if [ "$TERM" == "cygwin" ] ; then
   bind '"\e[5~":'$'"\C-E \C-A\C-Kcd_menu\n\C-Y\b"'
